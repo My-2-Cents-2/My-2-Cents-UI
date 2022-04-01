b9238a294577b2206743e6bf40f9f4f13c456394
Andrew DeMarco <97972359+andydemarks@users.noreply.github.com>
2022-04-01 11:52:42 -0400

    Updated tests for the component spec ts files and removed unnecessary code

--- My-2-Cents-UI/src/app/_services/account.service.spec.ts
+++ My-2-Cents-UI/src/app/_services/account.service.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AccountService } from './account.service';
 
@@ -6,11 +8,14 @@ describe('AccountService', () => {
   let service: AccountService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    });
     service = TestBed.inject(AccountService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
